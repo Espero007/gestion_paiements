b6d9847b29c16a61602a902f8982c99f0c1b4700
ifeleoncecomlan <ifeleoncecomlan@gmail.com>
2025-05-16 22:40:21 +0100

    Correction de la ligne 4 de selecttypephp

--- en_attente_integration/ife/select_type.php
+++ en_attente_integration/ife/select_type.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once(__DIR__ . '/variables.php');
+//require_once(__DIR__ . '/variables.php');
 //require_once(__DIR__ . '/header.php');
 
 $loggedUser = $_SESSION['loguser'] ?? null;
